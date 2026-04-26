d20f3c97dcd14a74d3772cd24ec4a654b245f37e
Gagandeep Singh <142520245+gdsKaran@users.noreply.github.com>
2026-04-26 16:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+document.getElementById('btn').addEventListener('click', function () {
+	  let name = document.getElementById("name").value;
+      let year = document.getElementById("year").value;
+	let url = "https://localhost:8080/";
+
+	if (name && year) {
+    url += `?name=${name}&year=${year}`;
+  } else if (name) {
+    url += `?name=${name}`;
+  } else if (year) {
+    url += `?year=${year}`;
+  }
+
+  document.getElementById("url").innerText = url;
+})
